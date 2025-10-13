09acfa7b75fd559b1300f9acd3e63d3bcdff5360
yudinee <apdkfldbwls@icloud.com>
2025-10-13 20:08:10 +0900

    TETO-95 메인페이지 레스토랑 키워도, 레스토랑 리스트 연결

---
+++ backend/restaurant-service/src/main/java/com/tabletopia/restaurantservice/domain/restaurantcaCategory/repository/RestaurnatCategoryRepository.java
@@ -0,0 +1,19 @@
+package com.tabletopia.restaurantservice.domain.restaurantcaCategory.repository;
+
+import com.tabletopia.restaurantservice.domain.restaurantcaCategory.entity.RestaurantCategory;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * 레스토랑 카테고리 리파지토리
+ *
+ * @author 성유진
+ * @since 2025-10-13
+ */
+
+public interface RestaurnatCategoryRepository extends JpaRepository<RestaurantCategory, Long> {
+
+  //OrderByDisplay 오름차순으로 리스트 찾기
+  List<RestaurantCategory>findAllByOrderByDisplayOrderAsc();
+
+}
---
+++ frontend/user/src/pages/utils/RestaurantCategory.jsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const URL="http://localhost:10022/api/restaurantcategories";
+
+export const getCategoryList=()=>
+    axios.get(URL); //카테고리 리스트
+
+export const getRestaurantByCategory=(id)=>
+    axios.get(`${URL}/${id}`);
