764e60ca59c1de1babeae68e0434e0ca918534a0
dev-Kiwi7 <seodalmi.u@gmail.com>
2025-10-14 15:23:41 +0900

    TETO 81 웨이팅 호출알림

--- frontend/user/src/components/header/DetailHeader.jsx
+++ frontend/user/src/components/header/DetailHeader.jsx
@@ -1,7 +1,19 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
+import NotificationPopup from '../NotificationPopup'
 
 export function DetailHeader() {
+    const [showNotificationPopup, setShowNotificationPopup] = useState(false)
+
+    const handleNotificationClick = () => {
+        setShowNotificationPopup(true)
+    }
+
+    const handleCloseNotificationPopup = () => {
+        setShowNotificationPopup(false)
+    }
+
     return (
         <>
             <div className={styles.breadcrumb}>홈 &gt; 강남 &gt; 일식 &gt; 정미스시</div>
@@ -9,9 +21,14 @@ export function DetailHeader() {
                 <button className="btn btn-outline">
                 <Link to = "/mypage" className={styles.linkNoDeco}>마이</Link>
                 </button>
-                <button className="btn btn-outline">🔗 공유</button>
+                <button className="btn btn-outline" onClick={handleNotificationClick}>🔗 알림</button>
                 <button className="btn btn-outline">❤️ 찜</button>
             </div>
+
+            <NotificationPopup
+                show={showNotificationPopup}
+                onClose={handleCloseNotificationPopup}
+            />
         </>
     )
 }
