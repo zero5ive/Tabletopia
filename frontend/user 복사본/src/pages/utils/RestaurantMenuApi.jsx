a048b571fe460db7499142b5afe93dfb74283b45
yudinee <apdkfldbwls@icloud.com>
2025-10-20 15:45:03 +0900

    TETO-126 레스토랑 리스트 페이징 오류 수정

--- frontend/user 복사본/src/pages/utils/RestaurantMenuApi.jsx
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-const URL="http://localhost:8002/api/user/restaurants";
-
-export const getRestaurantMenu=(restaurantId)=>
-    axios.get(`${URL}/${restaurantId}/menus`); 
