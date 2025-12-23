7c45b9e6179becf886302979c2806e71c30d6328
sh-Dang <henrys200632@gmail.com>
2025-12-23 18:30:56 +0900

    Settings: Docker 이미지 설정파일 생성, Next: MySQL 연동 필요

--- frontend/user/vite.config.js
+++ frontend/user/vite.config.js
@@ -4,18 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   server: {
-    port: 3000,  // 프론트엔드 포트
-    proxy: {
-      '/api': {
-        target: 'http://localhost:10022',  // 백엔드 서버 포트
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
-  // 전역타입 정의
   define: {
-    global: 'globalThis',
+  global: 'window', // global을 window로 정의
   },
 })
